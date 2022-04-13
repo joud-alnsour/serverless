75dc5aaf23463a2586c324430d4b2de199ef0e50
joud-alnsour <alnsour.joud@gmail.com>
2022-04-13 12:51:44 +0300

    done

--- api/calendar.py
+++ api/calendar.py
@@ -14,21 +14,17 @@ class handler(BaseHTTPRequestHandler):
     dic=dict(query_string)
     name= dic.get('name')
     if name:
-      message = f'Hello, {name}!\n'
+      message = f'Hi {name}!\n'
     else:
-      message = 'Hello, Stranger!\n' 
+      message = 'Hi Stranger\n' 
 
     message += f"\n Greetings from Python Version {platform.python_version()} at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n"
-    message1 = '\nThis is the datetime Year/Month/Day/Number of Week/Hour/Minute/Second: ' +'\n'
-    message2 = '\nThis is the Calendar for year 2022: ' +'\n\n'
-
+    
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     self.wfile.write(message.encode())
-    self.wfile.write(message1.encode())
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d/%W/ %H:%M:%S \n')).encode())
-    self.wfile.write(message2.encode())
     self.wfile.write(calendar.calendar(2022, 2, 1, 6).encode())
 
     return
